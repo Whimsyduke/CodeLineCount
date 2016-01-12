afacb85ab6febcd4c1104433525a6bc267fdefbe
Whimsy Elysium Duke <whimsyduke@hotmail.com>
2016-01-12 16:05:24 +0800

    2016.1.12：除统计自写内容版本。

---
+++ CodeLineCount/InformationItem.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CodeLineCount
+{
+    /// <summary>
+    /// InformationItem.xaml 的交互逻辑
+    /// </summary>
+    public partial class InformationItem : UserControl
+    {
+        public string FileText
+        {
+            get
+            {
+                return fileText;
+            }
+
+            set
+            {
+                fileText = value;
+            }
+        }
+        private string fileText;
+
+        public int TotalLines
+        {
+            get
+            {
+                return totalLines;
+            }
+
+            set
+            {
+                totalLines = value;
+            }
+        }        
+        private int totalLines = 0;
+
+        public int MineLines
+        {
+            get
+            {
+                return mineLines;
+            }
+
+            set
+            {
+                mineLines = value;
+            }
+        }
+        private int mineLines = 0;
+
+        public InformationItem()
+        {
+            InitializeComponent();
+        }
+
+        public InformationItem(FileInfo file, string parentPath)
+        {
+            InitializeComponent();
+            LoadFile(file);
+            Name.Content = file.Name;
+            Type.Content = file.Extension;
+            Path.Content = file.DirectoryName.Replace(parentPath, "");
+            TotalLines = FileText.Split('\n').Count();
+            Total.Content = TotalLines;
+        }
+
+        private void LoadFile(FileInfo file)
+        {
+            try
+            {
+                StreamReader streamReader = new StreamReader(file.FullName);
+                fileText = streamReader.ReadToEnd();
+                streamReader.Close();
+            }
+            catch
+            {
+                MessageBox.Show("无法打开文件" + file.FullName, "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ CodeLineCount/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace CodeLineCount
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SetPath_Click(object sender, RoutedEventArgs e)
+        {
+            string projectPath = Path.Text;
+            if (!Directory.Exists(projectPath))
+            {
+                projectPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            }
+            System.Windows.Forms.FolderBrowserDialog folderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            folderDialog.Description = "选择代码路径";
+            folderDialog.SelectedPath = projectPath;
+            folderDialog.ShowDialog();
+            if (folderDialog.SelectedPath != String.Empty)
+                Path.Text = folderDialog.SelectedPath;
+        }
+
+        private void Count_Click(object sender, RoutedEventArgs e)
+        {
+            List.Items.Clear();
+            if (!Directory.Exists(Path.Text))
+            {
+                MessageBox.Show("找不到路径对应的文件", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            CheckFileInDirectory(new DirectoryInfo(Path.Text), Path.Text);
+
+            int fileCount = 0, totalCount = 0, mineCount = 0;
+            foreach (InformationItem select in List.Items)
+            {
+                fileCount++;
+                totalCount += select.TotalLines;
+            }
+            Result.Content = "文件总数：" + fileCount.ToString() + "，总行数：" + totalCount.ToString() + "自己写行数：" + mineCount.ToString();
+        }
+
+        private void CheckFileInDirectory(DirectoryInfo dir, string parentPath)
+        {
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo select in files)
+            {
+                if (select.Extension == ".cc" || select.Extension == ".h")
+                    List.Items.Add(new InformationItem(select, parentPath));
+            }
+            DirectoryInfo[] folders = dir.GetDirectories();
+            foreach (DirectoryInfo select in folders)
+            {
+                CheckFileInDirectory(select, parentPath);
+            }
+        }
+    }
+}
